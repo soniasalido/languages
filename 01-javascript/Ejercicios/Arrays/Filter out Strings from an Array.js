80677847d912e1e2949b82a52acf9ae8b60e964e
soniasalido <soniasalido@gmail.com>
2024-08-06 13:11:54 +0200

    añadiendo algun ejercicio de arrays

--- 01-javascript/Ejercicios/Arrays/Filter out Strings from an Array.js
+++ 01-javascript/Ejercicios/Arrays/Filter out Strings from an Array.js
@@ -19,7 +19,17 @@ const filterArray = arr =>{
 }
 
 
+function filterArray2(arr) {
+    return arr.filter(num => Number.isInteger(num));
+}
+
+
 // Test Cases
 console.log(filterArray([1, 2, "a", "b"])); // ➞ [1, 2]
 console.log(filterArray([1, "a", "b", 0, 15])); // ➞ [1, 0, 15]
 console.log(filterArray([1, 2, "aasf", "1", "123", 123])); // ➞ [1, 2, 123]
+
+
+console.log(filterArray2([1, 2, "a", "b"])); // ➞ [1, 2]
+console.log(filterArray2([1, "a", "b", 0, 15])); // ➞ [1, 0, 15]
+console.log(filterArray2([1, 2, "aasf", "1", "123", 123])); // ➞ [1, 2, 123]
