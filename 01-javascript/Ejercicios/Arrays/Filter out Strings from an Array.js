27bcb155f1146f795dfc267db93b1621ed53dfab
soniasalido <soniasalido@gmail.com>
2024-08-06 13:09:56 +0200

    añadiendo algun ejercicio de arrays

---
+++ 01-javascript/Ejercicios/Arrays/Filter out Strings from an Array.js
@@ -0,0 +1,25 @@
+/*
+Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+
+Examples
+filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+
+filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+
+filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+ */
+
+
+const filterArray = arr =>{
+    let newArr = [];
+    arr.forEach( (element) => {
+        typeof element === 'number' && newArr.push(element);
+    });
+    return newArr;
+}
+
+
+// Test Cases
+console.log(filterArray([1, 2, "a", "b"])); // ➞ [1, 2]
+console.log(filterArray([1, "a", "b", 0, 15])); // ➞ [1, 0, 15]
+console.log(filterArray([1, 2, "aasf", "1", "123", 123])); // ➞ [1, 2, 123]
